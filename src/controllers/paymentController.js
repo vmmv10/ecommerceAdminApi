cf91c53291479fbfd6b04ceaee3370adaaf330e0
Victor Moya <vmanuelmoya@gmail..com>
2023-11-22 00:59:46 -0300

    update: Mercado pago

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,3 +1,53 @@
+import mercadopago from "mercadopago"
+
 export async function createOrder(req, res) {
-    res.json('Realizado con éxito', 200)
+  mercadopago.configure({
+    access_token: process.env.MERCADO_PAGO_TOKEN,
+  });
+  const result = await mercadopago.preferences.create({
+    items: [
+      {
+        title: "Laptop Lenovo",
+        unit_price: 500000,
+        currency_id: "CLP",
+        quantity: 1,
+      }
+    ],
+    back_urls: {
+      success: `${process.env.URL}/api/payment/success`,
+      failure: `${process.env.URL}/api/payment/failure`,
+      pending: `${process.env.URL}/api/payment/pending`,
+    },
+    notification_url: `${process.env.URL_SSL}/api/payment/webhook`,
+  })
+  console.log(result)
+  res.json('Realizado con éxito', 200)
+}
+
+export async function orderSuccess(req, res) {
+  res.json('Realizado con éxito', 200)
+}
+
+export async function orderFailure(req, res) {
+  res.json('Error al realizar la operación', 200)
+}
+
+export async function orderPending(req, res) {
+  res.json('Esperando una respuesta', 200)
+}
+
+export async function recieveWebhook(req, res) {
+  try {
+    const payment = req.query;
+    console.log(payment);
+    if (payment.type === "payment") {
+      const data = await mercadopago.payment.findById(payment["data.id"]);
+      console.log(data);
+    }
+
+    res.status(204);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something goes wrong" });
   }
+}
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createOrder } from "../controllers/paymentController.js";
+import { createOrder, orderSuccess, orderFailure, orderPending, recieveWebhook } from "../controllers/paymentController.js";
 
 const router = Router();
 
-router.post('/create-order', createOrder);
+router.post('/payment/create-order', createOrder);
+router.get('/payment/success', orderSuccess);
+router.get('/payment/failure', orderFailure);
+router.get('/payment/pending', orderPending);
+router.post('/payment/webhook', recieveWebhook);
 
 export default router
